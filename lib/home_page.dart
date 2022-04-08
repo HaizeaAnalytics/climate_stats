b241aa276cc2d4058f419363d6e3995c4d4db40e
OKemmis <40152619+OKemmis@users.noreply.github.com>
2022-04-09 00:04:42 +1000

    Add sign out function

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,26 @@
+import 'package:climate_stats/authentication_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text("HOME"),
-    ));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Text("HOME"),
+            ElevatedButton(
+              onPressed: () {
+                context.read<AuthenticationService>().signOut();
+              },
+              child: const Text("Sign out"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
