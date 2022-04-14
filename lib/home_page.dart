5cb71c647e0312e4b6a8e68dde8ca3c277ed7782
Gong Chen <gchen@Gongs-MacBook-Pro.local>
2022-04-14 11:46:02 +1000

    update home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,24 +1,38 @@
 import 'package:climate_stats/authentication_service.dart';
 import 'package:climate_stats/app_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'profile_page.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+const String databaseName = "userInfo";
+String userEmail = "";
+
 class HomePage extends StatelessWidget {
   //const HomePage({Key? key}) : super(key: key);
   User userInfo;
-  HomePage(this.userInfo) {}
+  String lastLoginTime = "";
+
+  HomePage(this.userInfo) {
+    userEmail = userInfo.email.toString();
+    lastLoginTime = userInfo.metadata.lastSignInTime!.toLocal().toString();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar('Home'),
-      body: Center(
+      body: Container(
         child: Column(
           children: [
-            const Text("HOME"),
+            LogoArea(),
+            PageInfo(this.lastLoginTime),
+            /**
+             * 
+             * Add button to pages for development, if the navigation bar has not been developed.
+             */
             ElevatedButton(
               onPressed: () {
                 context.read<AuthenticationService>().signOut();
@@ -35,9 +49,118 @@ class HomePage extends StatelessWidget {
               },
               child: const Text("Profile Page"),
             ),
+            /*************/
+
+            Row(
+              children: [
+                // Todo: finding region and favourite history
+
+                Container(
+                  width: 600,
+                  height: 600,
+                  child: CustomPaint(
+                    painter: OpenPainter(),
+                  ),
+                ),
+
+                // Todu: map conponent
+                Container(
+                  width: 600,
+                  height: 600,
+                  child: CustomPaint(
+                    painter: OpenPainter(),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("../assets/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        width: double.infinity,
       ),
     );
   }
+}
+
+/*
+Logo area
+ */
+class LogoArea extends StatelessWidget {
+  const LogoArea({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Image.asset('../assets/haizea.png', fit: BoxFit.cover),
+        ],
+      ),
+      height: 68.0,
+      width: double.infinity,
+    );
+  }
+}
+
+/*
+Page information
+ */
+class PageInfo extends StatelessWidget {
+  //const PageInfo({Key? key}) : super(key: key);
+
+  String lastLoginTime;
+  String username = "";
+
+  PageInfo(this.lastLoginTime) {
+    FirebaseFirestore.instance
+        .collection(databaseName)
+        .doc(userEmail)
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        username = documentSnapshot.get("firstname");
+        print(username);
+      } else {
+        print('Document does not exist on the database');
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text("Welcome, " + username,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 54.0,
+                color: Colors.white,
+              )),
+          Text(
+            "Last login: " + lastLoginTime,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OpenPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Color.fromARGB(255, 242, 239, 241)
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset(80, 1) & const Size(500, 500), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
