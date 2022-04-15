9d3c59f9d2d456683749a914bf64f92889afefdc
Hangzhi Shi <shz1996@outlook.com>
2022-04-15 11:22:34 +0800

    Add UI Elements to HomePage:Favourite list, Map, search bar. Interaction not implemented yet.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:climate_stats/authentication_service.dart';
 import 'package:climate_stats/app_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'profile_page.dart';
 
@@ -11,9 +13,16 @@ const String databaseName = "userInfo";
 String userEmail = "";
 
 class HomePage extends StatelessWidget {
-  //const HomePage({Key? key}) : super(key: key);
+  // const HomePage({Key? key}) : super(key: key);
+
   User userInfo;
   String lastLoginTime = "";
+
+  List<String> favourites = [
+    '12 Swanston Street, Joel Joel',
+    '8 Lapko Road, Magtitup',
+    '10 Downing Street, London'
+  ];
 
   HomePage(this.userInfo) {
     userEmail = userInfo.email.toString();
@@ -29,8 +38,147 @@ class HomePage extends StatelessWidget {
           children: [
             LogoArea(),
             PageInfo(this.lastLoginTime),
+
+            /*************/
+            Container(
+              width: 500,
+              height: 50,
+              child: TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                    labelText: "Finds somewhere new.",
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    filled: true,
+                    fillColor: Colors.grey[300],
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: const BorderSide(width: 0, style: BorderStyle.none)),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text("Favourites",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                      color: Colors.white,
+                    ),
+                ),
+                SizedBox(width: 400,),
+                Text("Maps",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                      color: Colors.white,
+                    ),
+                ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 500,
+                  height: 500,
+                  child: ListView.builder(
+                    itemCount: favourites.length,
+                    // itemExtent: 20.0,
+                    itemBuilder: (context, index){
+                      return Card(
+                        child: ListTile(
+                          title: TextButton(
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(favourites[index]),
+                              ],
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon:Icon(Icons.delete_forever),
+                            onPressed: (){},
+                          ),
+                        ),
+                      );
+                    }
+                  ),
+                ),
+
+                const SizedBox(width: 20),
+
+                SizedBox(
+                  width: 500,
+                  height: 500,
+                  child: FlutterMap(
+                    options: MapOptions(
+                      center: latLng.LatLng(-35.28,149.13),
+                      zoom: 16.0,
+                    ),
+                    layers: [
+                      MarkerLayerOptions(
+                        markers: [
+                          Marker(
+                            width: 20.0,
+                            height: 20.0,
+                            point: latLng.LatLng(-35.28,149.13),
+                            builder: (ctx) =>
+                                Container(
+                                  child: FlutterLogo(),
+                                ),
+                          ),
+                        ],
+                      ),
+                    ],
+                    children: <Widget>[
+                      TileLayerWidget(options: TileLayerOptions(
+                          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                          subdomains: ['a', 'b', 'c']
+                      )),
+                      MarkerLayerWidget(options: MarkerLayerOptions(
+                        markers: [
+                          Marker(
+                            width: 20.0,
+                            height: 20.0,
+                            point: latLng.LatLng(-35.28,149.13),
+                            builder: (ctx) =>
+                                Container(
+                                  child: FlutterLogo(),
+                                ),
+                          ),
+                        ],
+                      )),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 50,
+                  height: 500,
+                  child: Scaffold(
+                    backgroundColor: const Color.fromRGBO(0, 0, 0, 0),
+                    floatingActionButton: FloatingActionButton(
+                      onPressed: () {
+                        // Add your onPressed code here!
+                      },
+                      child: const Icon(Icons.add),
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 10),
             /**
-             * 
+             *
              * Add button to pages for development, if the navigation bar has not been developed.
              */
             ElevatedButton(
@@ -49,30 +197,6 @@ class HomePage extends StatelessWidget {
               },
               child: const Text("Profile Page"),
             ),
-            /*************/
-
-            Row(
-              children: [
-                // Todo: finding region and favourite history
-
-                Container(
-                  width: 600,
-                  height: 600,
-                  child: CustomPaint(
-                    painter: OpenPainter(),
-                  ),
-                ),
-
-                // Todu: map conponent
-                Container(
-                  width: 600,
-                  height: 600,
-                  child: CustomPaint(
-                    painter: OpenPainter(),
-                  ),
-                ),
-              ],
-            ),
           ],
         ),
         decoration: const BoxDecoration(
@@ -87,71 +211,87 @@ class HomePage extends StatelessWidget {
   }
 }
 
-/*
-Logo area
- */
-class LogoArea extends StatelessWidget {
-  const LogoArea({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Image.asset('../assets/haizea.png', fit: BoxFit.cover),
-        ],
-      ),
-      height: 68.0,
-      width: double.infinity,
-    );
-  }
-}
+// /**
+//  * Favourite list
+//  */
+// class FavList extends StatelessWidget{
+//   const FavList({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       itemCount: 100,
+//       itemExtent: 30,
+//       itemBuilder: ,
+//     );
+//   }
+// }
 
-/*
-Page information
- */
-class PageInfo extends StatelessWidget {
-  //const PageInfo({Key? key}) : super(key: key);
+// /*
+// Logo area
+//  */
+// class LogoArea extends StatelessWidget {
+//   const LogoArea({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Row(
+//         children: [
+//           Image.asset('../assets/haizea.png', fit: BoxFit.cover),
+//         ],
+//       ),
+//       height: 68.0,
+//       width: double.infinity,
+//     );
+//   }
+// }
 
-  String lastLoginTime;
-  String username = "";
-
-  PageInfo(this.lastLoginTime) {
-    FirebaseFirestore.instance
-        .collection(databaseName)
-        .doc(userEmail)
-        .get()
-        .then((DocumentSnapshot documentSnapshot) {
-      if (documentSnapshot.exists) {
-        username = documentSnapshot.get("firstname");
-        print(username);
-      } else {
-        print('Document does not exist on the database');
-      }
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text("Welcome, " + username,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 54.0,
-                color: Colors.white,
-              )),
-          Text(
-            "Last login: " + lastLoginTime,
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
+// /*
+// Page information
+//  */
+// class PageInfo extends StatelessWidget {
+//   //const PageInfo({Key? key}) : super(key: key);
+//
+//   String lastLoginTime;
+//   String username = "";
+//
+//   PageInfo(this.lastLoginTime) {
+//     FirebaseFirestore.instance
+//         .collection(databaseName)
+//         .doc(userEmail)
+//         .get()
+//         .then((DocumentSnapshot documentSnapshot) {
+//       if (documentSnapshot.exists) {
+//         username = documentSnapshot.get("firstname");
+//         print(username);
+//       } else {
+//         print('Document does not exist on the database');
+//       }
+//     });
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Column(
+//         children: [
+//           Text("Welcome, " + username,
+//               style: const TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 54.0,
+//                 color: Colors.white,
+//               )),
+//           Text(
+//             "Last login: " + lastLoginTime,
+//             style: const TextStyle(
+//               color: Colors.white,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
 class OpenPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
